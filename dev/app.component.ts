b70614253088258163123aa306719e5cf12f96bd
aayusharora <aayush113002@gmail.com>
2017-10-19 06:02:46 +0530

    Added custom component

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,11 +1,14 @@
 import {Component} from 'angular2/core';
+import {MainComponent} from './main.component'
 
 @Component({
     selector: 'my-app',
     template: `
         <h1>Angular 2 Boilerplate</h1>
         <p>Hello World!</p>
+        <main></main>
     `,
+    directives: [ MainComponent]
 })
 export class AppComponent {
 
---
+++ dev/main.component.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by aayusharora on 10/19/17.
+ */
+
+import {Component} from 'angular2/core';
+
+@Component ({
+    selector: 'main',
+    template: `<h1>
+               AAAAAAAAAAAAA
+               </h1>`
+})
+
+export class MainComponent {
+
+}
