b48f3ec01de23aa186ec4256f0244e24f04f7c36
aayusharora <aayush113002@gmail.com>
2017-10-21 23:06:40 +0530

    Added quickstart Angular2 app

---
+++ CodingProjects/Angular-Concepts/my-app/src/app/str.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by aayusharora on 10/21/17.
+ */
+import {Component} from '@angular/core'
+
+@Component({
+  selector: 'str',
+  template: ` {{'Hello'}}`
+})
+
+export class StrComponent{
+
+}
